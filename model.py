af7761602c9c0752833dcc83251aa1dfc052dc72
panditrahulsharma <rahulpandit151197@gmail.com>
2019-09-29 15:10:26 +0530

    cleaning data

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ df=pd.read_csv("Train.csv")
 df=df[1:10]
 p=df['Col3']
 df.isnull().any(axis=0)
-
+df1=pd.DataFrame()
 for i in range(3,999):
     Check_Nan=(df['Col{}'.format(i)].isnull().any(axis=0))
     #if Nan value found then if condition true
@@ -18,5 +18,18 @@ for i in range(3,999):
         print(df['Col{}'.format(i)])
         
         
-        
-df['Col139'].notnull().any(axis=0)    
+for i in range(3,len(df)):
+    p=all(df['Col138'].isnull())
+    if p :
+        df1['Col{}'.format(i)]=df.drop(['Col{}'.format(i)],axis=1)
+
+
+
+
+df['Col13'].isnull()   
+
+p=all(df['Col138'].isnull())
+p
+
+#first remove those columns that contains only 
+df=df.drop(['Col1'],axis=1)
