5ec6c3afafc2fa7c850ddf663381ef88a87aacb1
panditrahulsharma <rahulpandit151197@gmail.com>
2019-09-29 16:28:28 +0530

    cleaning data

--- model.py
+++ model.py
@@ -4,32 +4,40 @@ import pandas as pd
 df=pd.read_csv("Train.csv")
 
 df=df[1:10]
-p=df['Col3']
-df.isnull().any(axis=0)
-df1=pd.DataFrame()
+
+#remove those columns that have 70% null values 
 for i in range(3,999):
-    Check_Nan=(df['Col{}'.format(i)].isnull().any(axis=0))
-    #if Nan value found then if condition true
-    if  Check_Nan:
-        print("****************before****************")
-        print(df['Col{}'.format(i)])
-        df['Col{}'.format(i)]=df['Col{}'.format(i)].fillna(np.mean(df['Col{}'.format(i)]))
-        print("************after*********************")
-        print(df['Col{}'.format(i)])
+    try:
+        sum_Nan=df['Col{}'.format(i)].isnull().sum()
+        #70% data nan then remove columns
+        #
+        x70=(17521*7)/10
+        x50=(17521*5)/10
+        #if Nan value found then if condition true
+        if sum_Nan >=x70:
+            df=df.drop(['Col{}'.format(i)],axis=1)
+           #if columns have morethan 50% data nan value and less 70% then  direct fill with 0 
+        elif  sum_Nan <x70 and sum_Nan>x50:
+            df['Col{}'.format(i)]=df['Col{}'.format(i)].fillna(np.mean(0))
+
+        elif sum_Nan<x50:
+            #then fill mean values
+            df['Col{}'.format(i)]=df['Col{}'.format(i)].fillna(np.mean(df['Col{}'.format(i)]))
+
+
+    except KeyError:
+        pass
+
+
         
         
-for i in range(3,len(df)):
-    p=all(df['Col138'].isnull())
-    if p :
-        df1['Col{}'.format(i)]=df.drop(['Col{}'.format(i)],axis=1)
+for i in range(3,2395):
+    try:
+        p=all(df['Col{}'.format(i)].isnull())
+        if p :
+            df=df.drop(['Col{}'.format(i)],axis=1)
+    except KeyError:
+        pass
 
 
-
-
-df['Col13'].isnull()   
-
-p=all(df['Col138'].isnull())
-p
-
-#first remove those columns that contains only 
-df=df.drop(['Col1'],axis=1)
+df['Col37'].isnull().sum()
