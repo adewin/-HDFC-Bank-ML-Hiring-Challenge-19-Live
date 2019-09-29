78055b1692a1bebd267267755f3dfc8e82e752a3
panditrahulsharma <rahulpandit151197@gmail.com>
2019-09-29 12:17:31 +0530

    cleaning data

--- model.py
+++ model.py
@@ -8,4 +8,11 @@ p=df['Col3']
 df.isnull().any(axis=0)
 
 for i in range(3,20):
-    print(df['Col{}'.format(i)].isnull().any(axis=0))
+    Check_Nan=(df['Col{}'.format(i)].isnull().any(axis=0))
+    #if Nan value found then if condition true
+    if  Check_Nan:
+        print("****************before****************")
+        print(df['Col{}'.format(i)])
+        df['Col{}'.format(i)]=df['Col{}'.format(i)].fillna(np.mean(df['Col{}'.format(i)]))
+        print("************after*********************")
+        print(df['Col{}'.format(i)])
