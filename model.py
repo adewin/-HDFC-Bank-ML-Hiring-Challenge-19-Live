94a9899dc30ab807b6dc5c4696bf37bb74723b34
panditrahulsharma <rahulpandit151197@gmail.com>
2019-09-29 11:56:48 +0530

    cleaning data

--- model.py
+++ model.py
@@ -1,3 +1,11 @@
-r="rahul"
+import numpy as np
+import pandas as pd
 
-string = "".join((str(p))
+df=pd.read_csv("Train.csv")
+
+df=df[1:10]
+p=df['Col3']
+df.isnull().any(axis=0)
+
+for i in range(3,20):
+    print(df['Col{}'.format(i)].isnull().any(axis=0))
